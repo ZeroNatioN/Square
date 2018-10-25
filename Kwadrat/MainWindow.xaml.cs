97593e6aa1e09b2e13941d4c23a105b10273db8f
Michał Krause <michal.krause01@gmail.com>
2018-10-25 17:56:33 +0200

    Added main functionality
    -Computing Area and Circuit based on edge length provided by user
    
    -Drawing rectangle with edge length, colour and translucency specified by user
    
    -Hiding and Showing drawn rectangle with radio buttons

--- Kwadrat/MainWindow.xaml.cs
+++ Kwadrat/MainWindow.xaml.cs
@@ -24,5 +24,55 @@ namespace Kwadrat
         {
             InitializeComponent();
         }
+
+        private void TxtEdge_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double edgeLength;
+            if (double.TryParse(TxtEdge.Text, out edgeLength) && edgeLength >= 0)
+            {
+                TxtArea.Text = Math.Pow(edgeLength, 2.0).ToString();
+                TxtCircuit.Text = (edgeLength * 4).ToString();
+                LblMessage.Content = String.Empty;
+            }
+            else
+            {
+                LblMessage.Content = "Enter positive number";
+            }
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            TxtEdge.Text = String.Empty;
+            TxtArea.Text = String.Empty;
+            TxtCircuit.Text = String.Empty;
+            LblMessage.Content = "Enter edge length";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TxtEdge.Text, out var edge) && edge <=380)
+            {
+                Rectangle.Height = edge;
+                Rectangle.Width = edge;
+                SolidColorBrush colour = (SolidColorBrush) new BrushConverter().ConvertFromString(CmbColours.Text);
+                Rectangle.Stroke = colour;
+                Rectangle.Fill = colour;
+                Rectangle.Opacity = (ChkbTranslucent.IsChecked.Value) ? 0.5 : 1;
+            }
+            else
+            {
+                LblMessage.Content = "No data or the edge is to big";
+            }
+        }
+
+        private void RbtnHide_Checked(object sender, RoutedEventArgs e)
+        {
+            Rectangle.Visibility = Visibility.Hidden;
+        }
+
+        private void RbtnShow_Checked(object sender, RoutedEventArgs e)
+        {
+            Rectangle.Visibility = Visibility.Visible;
+        }
     }
 }
